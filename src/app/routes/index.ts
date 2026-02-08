12236c95e070c1d369a1ed465430ee319d049acf
a1shuvo <a1shuvosaha@gmail.com>
2026-02-08 19:02:58 +0600

    added getAllDoctors api

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { UserRoutes } from "../module/user/user.route";
 
@@ -8,5 +9,6 @@ const router = Router();
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes);
 router.use("/users", UserRoutes);
+router.use("/doctors", DoctorRoutes);
 
 export const IndexRoutes = router;
