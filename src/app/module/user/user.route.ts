4f31176282194d2a108f4007624216d561ebafa1
a1shuvo <a1shuvosaha@gmail.com>
2026-02-08 20:49:16 +0600

    added validateRequest middleware and create doctor schema validation using zod

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import { validateRequest } from "../../middleware/validateRequest";
 import { UserController } from "./user.controller";
+import { createDoctorZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/create-doctor", UserController.createDoctor);
+router.post(
+  "/create-doctor",
+  validateRequest(createDoctorZodSchema),
+  UserController.createDoctor,
+);
 
 export const UserRoutes = router;
