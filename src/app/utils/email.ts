05b8c9c6e7aa6cfc419716e1c6153a94f9dab8f2
a1shuvo <a1shuvosaha@gmail.com>
2026-02-15 19:38:28 +0600

    added email sender with nodemailer and google cloud

---
+++ src/app/utils/email.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import ejs from "ejs";
+import status from "http-status";
+import nodemailer from "nodemailer";
+import path from "path";
+import { envVars } from "../config/env";
+import AppError from "../errorHelplers/AppError";
+
+const transporter = nodemailer.createTransport({
+  host: envVars.EMAIL_SENDER.SMTP_HOST,
+  port: Number(envVars.EMAIL_SENDER.SMTP_PORT),
+  secure: true,
+  auth: {
+    user: envVars.EMAIL_SENDER.SMTP_USER,
+    pass: envVars.EMAIL_SENDER.SMTP_PASS,
+  },
+});
+
+interface SendEmailOptions {
+  to: string;
+  subject: string;
+  templateName: string;
+  templateData: Record<string, any>;
+  attachments?: {
+    fileName: string;
+    content: Buffer | string;
+    contentType: string;
+  }[];
+}
+
+export const sendEmail = async ({
+  to,
+  subject,
+  templateName,
+  templateData,
+  attachments,
+}: SendEmailOptions) => {
+  try {
+    const templatePath = path.resolve(
+      process.cwd(),
+      `src/app/templates/${templateName}.ejs`,
+    );
+    const html = await ejs.renderFile(templatePath, templateData);
+
+    const info = await transporter.sendMail({
+      from: envVars.EMAIL_SENDER.SMTP_FROM,
+      to,
+      subject,
+      html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.fileName,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    });
+
+    console.log(`Email sent to ${to} : ${info.messageId}`);
+  } catch (error: any) {
+    console.log("Email sending error: ", error.message);
+    throw new AppError(status.INTERNAL_SERVER_ERROR, "Failed to send email!");
+  }
+};
