824561aa0afa2b08eb5ee4bebc215474009ee46d
a1shuvo <a1shuvosaha@gmail.com>
2026-02-17 21:05:42 +0600

    set up multer cloudinary for image upload

--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { Role } from "../../../generated/prisma/enums";
+import { multerUpload } from "../../config/multer.config";
 import { checkAuth } from "../../middleware/checkAuth";
 import { SpecialtyController } from "./specialty.controller";
 
 const router = Router();
 
+router.get("/", SpecialtyController.getAllSpecialties);
 router.post(
   "/",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single("file"),
   SpecialtyController.createSpecialty,
 );
-router.get("/", SpecialtyController.getAllSpecialties);
 router.patch(
   "/:id",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
