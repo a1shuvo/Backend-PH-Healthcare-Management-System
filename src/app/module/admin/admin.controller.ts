6271d16e151d3531721aa1560bf843c846a2845b
a1shuvo <a1shuvosaha@gmail.com>
2026-03-03 22:52:13 +0600

    created change user status and user role api

--- src/app/module/admin/admin.controller.ts
+++ src/app/module/admin/admin.controller.ts
@@ -10,19 +10,20 @@ const getAllAdmins = catchAsync(async (req: Request, res: Response) => {
   sendResponse(res, {
     httpStatusCode: status.OK,
     success: true,
-    message: "Admins fetched successfully!",
+    message: "Admins fetched successfully",
     data: result,
   });
 });
 
 const getAdminById = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
+
   const admin = await AdminService.getAdminById(id as string);
 
   sendResponse(res, {
     httpStatusCode: status.OK,
     success: true,
-    message: "Admin fetched successfully!",
+    message: "Admin fetched successfully",
     data: admin,
   });
 });
@@ -30,12 +31,13 @@ const getAdminById = catchAsync(async (req: Request, res: Response) => {
 const updateAdmin = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const payload = req.body;
+
   const updatedAdmin = await AdminService.updateAdmin(id as string, payload);
 
   sendResponse(res, {
     httpStatusCode: status.OK,
     success: true,
-    message: "Admin updated successfully!",
+    message: "Admin updated successfully",
     data: updatedAdmin,
   });
 });
@@ -43,19 +45,46 @@ const updateAdmin = catchAsync(async (req: Request, res: Response) => {
 const deleteAdmin = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const user = req.user;
+
   const result = await AdminService.deleteAdmin(id as string, user);
 
   sendResponse(res, {
     httpStatusCode: status.OK,
     success: true,
-    message: "Admin deleted successfully!",
+    message: "Admin deleted successfully",
+    data: result,
+  });
+});
+
+const changeUserStatus = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  const payload = req.body;
+  const result = await AdminService.changeUserStatus(user, payload);
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "User status changed successfully",
+    data: result,
+  });
+});
+
+const changeUserRole = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  const payload = req.body;
+  const result = await AdminService.changeUserRole(user, payload);
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "User role changed successfully",
     data: result,
   });
 });
 
 export const AdminController = {
   getAllAdmins,
-  getAdminById,
   updateAdmin,
   deleteAdmin,
+  getAdminById,
+  changeUserStatus,
+  changeUserRole,
 };
--- src/app/module/admin/admin.interface.ts
+++ src/app/module/admin/admin.interface.ts
@@ -1,7 +1,19 @@
-export interface IUpdateAdminPlayload {
+import { Role, UserStatus } from "../../../generated/prisma/enums";
+
+export interface IUpdateAdminPayload {
   admin?: {
     name?: string;
     profilePhoto?: string;
     contactNumber?: string;
   };
+}
+
+export interface IChangeUserStatusPayload {
+  userId: string;
+  userStatus: UserStatus;
+}
+
+export interface IChangeUserRolePayload {
+  userId: string;
+  role: Role;
 }
